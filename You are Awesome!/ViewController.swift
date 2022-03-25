3f0ea0fd4148ce571235f41d8e12465e6595f00c
Juan Jose Colin Arciniega <juan.jose.colin@gmail.com>
2022-03-24 18:53:57 -0600

    If statements

--- You are Awesome!/ViewController.swift
+++ You are Awesome!/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var messageLabel: UILabel!
     
     @IBOutlet weak var imageView: UIImageView!
@@ -18,11 +18,20 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         print("👍 View did load")
     }
-
+    
     @IBAction func messageButtonPressed(_ sender: UIButton) {
         print("😎 The message button was pressed")
-        messageLabel.text="You are Awesome!"
-        imageView.image=UIImage(named: "MM-TV-Top10-Moments-From-RuPauls-Drag-Race-Season-11_O6R2C4-1080p30_480")
+        
+        
+        if messageLabel.text=="You are Awesome!"{
+            messageLabel.text="You are Great!"
+            imageView.image=UIImage(named: "6263b52ad85b74e780c5f37e085c343e")
+            
+        } else {
+            messageLabel.text="You are Awesome!"
+            imageView.image=UIImage(named: "MM-TV-Top10-Moments-From-RuPauls-Drag-Race-Season-11_O6R2C4-1080p30_480")
+        }
+        
         
     }
     
