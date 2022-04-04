1b9fc1a65e112dbc396e861e0c4f784c523d9cbd
Juan Jose Colin Arciniega <juan.jose.colin@gmail.com>
2022-04-04 16:48:53 -0500

    Repeat-While

--- You are Awesome!/ViewController.swift
+++ You are Awesome!/ViewController.swift
@@ -12,8 +12,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     
-    var imageNumber = 0
-    var messageNumber = 0
+    var imageNumber = -1
+    var messageNumber = -1
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,39 +25,28 @@ class ViewController: UIViewController {
         
         let messages = ["Du bist Grossartig!","Eres lo Maximo!","When they need help, they always call you","You are the Best!", "You are Spectacular!","Nobody can do it better than you!"]
         
-        messageLabel.text=messages[Int.random(in: 0...messages.count-1)]
-        imageNumber = Int.random(in: 1...3)
-//        messageLabel.text=messages[messageNumber]
-//        messageNumber += 1
-//        if messageNumber == messages.count{
-//            messageNumber = 0
-//        }
+        var newMessageNumber: Int
+        var newImageNumber:Int
+        var newImage:UIImage
         
-//        print(imageNumber)
-//
-//        imageNumber = imageNumber + 1
-//
-//        if imageNumber > 3 {
-//            imageNumber = 1
-//        }
+        //Mensajes
+        repeat{
+            newMessageNumber=Int.random(in: 0...messages.count-1)
+        }while newMessageNumber == messageNumber
         
-//        let imageName = "0\(imageNumber)"   //intepolation of a variable value//
+        messageNumber=newMessageNumber
+        messageLabel.text=messages[messageNumber]
         
-        imageView.image=UIImage(named: "0" + String(imageNumber))   //Casting an Int to String
-//        imageView.image=UIImage(named: imageName)
-//        print("😎 The message button was pressed")
-//
-//        let awesomeMessage = "You are Awesome!"
-//
-//
-//        if messageLabel.text==awesomeMessage{
-//            messageLabel.text="You are Great!"
-//            imageView.image=UIImage(named: "02")
-//
-//        } else {
-//            messageLabel.text=awesomeMessage
-//            imageView.image=UIImage(named: "03")
-//        }
+        //Imagenes
+        repeat{
+            newImageNumber = Int.random(in: 1...3)
+        }while newImageNumber==imageNumber
+        
+        imageNumber=newImageNumber
+        newImage = UIImage(named: "0" + String(imageNumber))!
+        imageView.image=newImage
+        
+
     }
     
 }
